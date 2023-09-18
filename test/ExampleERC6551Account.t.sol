807b9e612486c65c46821089434e9f636fb4954f
Tomoya Kobayashi <tomocha.marika@gmail.com>
2023-09-18 13:03:46 +0900

    succeeded to test deploy

--- test/ExampleERC6551Account.t.sol
+++ test/ExampleERC6551Account.t.sol
@@ -2,43 +2,66 @@ pragma solidity ^0.8.0;
 import "forge-std/Test.sol";
 import "../src/ExampleERC6551Account.sol";
 import "../src/Counter.sol";
-import "openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
-
-contract MockERC721 is ERC721 {
-    constructor() ERC721("MockERC721", "MockERC721") {}
-
-    function mint(address to, uint256 tokenId) external {
-        _mint(to, tokenId);
-    }
-}
+import "./mocks/MockERC721.sol";
+import "./mocks/MockERC6551Account.sol";
+import "../src/ERC6551Registry.sol";
 
 contract ExampleERC6551AccountTest is Test {
-    ExampleERC6551Account public account;
+    ExampleERC6551Account public implementation;
+    ERC6551Registry public registry;
     MockERC721 public mockToken;
-    address nftOwner = address(1);
-    address receipientAddress = address(2);
-    uint256 tokenId = 1;
 
-    function setup() public {
-        mockToken = new MockERC721();
-        mockToken.mint(nftOwner, tokenId);
-        account = new ExampleERC6551Account();
+    function setUp() public {
+        registry = new ERC6551Registry();
+        implementation = new ExampleERC6551Account();
     }
 
-    function testExecute() public {
-        uint256 value = 0;
-        bytes memory data = abi.encodeWithSelector(
-            IERC721.transferFrom.selector,
-            nftOwner,
-            receipientAddress,
-            tokenId
-        );
-        bytes memory result = account.execute(
+    function testDeploy() public {
+        address deployedAccount = registry.createAccount(
+            address(implementation),
+            block.chainid,
             address(mockToken),
-            value,
-            data,
-            0
+            1,
+            12345,
+            ""
         );
-        assertEq(result, "");
+        assertTrue(deployedAccount != address(0));
+
+        address predictedAccount = registry.account(
+            address(implementation),
+            block.chainid,
+            address(mockToken),
+            1,
+            12345
+        );
+
+        assertEq(deployedAccount, predictedAccount);
     }
+
+    // function testToken() public {
+    //     (
+    //         uint256 chainId,
+    //         address tokenContract,
+    //         uint256 returnedTokenId
+    //     ) = account.token();
+
+    //     assertEq(chainId, block.chainid, "ChainId does not match");
+    // }
+
+    // function testExecute() public {
+    //     uint256 value = 0;
+    //     bytes memory data = abi.encodeWithSelector(
+    //         IERC721.transferFrom.selector,
+    //         nftOwner,
+    //         receipientAddress,
+    //         tokenId
+    //     );
+    //     bytes memory result = account.execute(
+    //         address(mockToken),
+    //         value,
+    //         data,
+    //         0
+    //     );
+    //     assertEq(result, "");
+    // }
 }
