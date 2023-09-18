3027d146ddff2ff4b50c08f5f55d7048b1b7c7f9
Tomoya Kobayashi <tomocha.marika@gmail.com>
2023-09-18 11:28:42 +0900

    add registry

---
+++ src/IERC6551Registry.sol
@@ -0,0 +1,27 @@
+interface IERC6551Registry {
+    event AccountCreated(
+        address account,
+        address indexed implementation,
+        uint256 chainId,
+        address indexed tokenContract,
+        uint256 indexed tokenId,
+        uint256 salt
+    );
+
+    function createAccount(
+        address implementation,
+        uint256 chainId,
+        address tokenContract,
+        uint256 tokenId,
+        uint256 salt,
+        bytes calldata initData
+    ) external returns (address account);
+
+    function account(
+        address implementation,
+        uint256 chainId,
+        address tokenContract,
+        uint256 tokenId,
+        uint256 salt
+    ) external view returns (address);
+}
