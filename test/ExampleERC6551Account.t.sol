adcd7462f82f3f2d970d7b8329139bf9e9b2092f
Tomoya Kobayashi <tomocha.marika@gmail.com>
2023-09-23 12:27:01 +0900

    succeeded to test

--- test/ExampleERC6551Account.t.sol
+++ test/ExampleERC6551Account.t.sol
@@ -1,4 +1,6 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+
 import "forge-std/Test.sol";
 import "../src/ExampleERC6551Account.sol";
 import "../src/Counter.sol";
@@ -9,7 +11,7 @@ import "../src/ERC6551Registry.sol";
 contract ExampleERC6551AccountTest is Test {
     ExampleERC6551Account public implementation;
     ERC6551Registry public registry;
-    MockERC721 public nft;
+    MockERC721 nft = new MockERC721();
 
     function setUp() public {
         registry = new ERC6551Registry();
@@ -39,15 +41,31 @@ contract ExampleERC6551AccountTest is Test {
     }
 
     function testExecute() public {
-        nft.mint(address(2), 3);
-        // address account = registry.createAccount(
-        //     address(implementation),
-        //     block.chainid,
-        //     address(nft),
-        //     1,
-        //     0,
-        //     ""
-        // );
-        // assertTrue(account != address(0));
+        nft.mint(vm.addr(1), 1);
+        address account = registry.createAccount(
+            address(implementation),
+            block.chainid,
+            address(nft),
+            1,
+            0,
+            ""
+        );
+        assertTrue(account != address(0));
+
+        IERC6551Account accountInstance = IERC6551Account(payable(account));
+        IERC6551Executable executableAccountInstance = IERC6551Executable(
+            account
+        );
+        vm.deal(account, 1 ether);
+        vm.prank(vm.addr(1));
+        executableAccountInstance.execute(
+            payable(vm.addr(2)),
+            0.5 ether,
+            "",
+            0
+        );
+        assertEq(account.balance, 0.5 ether);
+        assertEq(vm.addr(2).balance, 0.5 ether);
+        assertEq(accountInstance.state(), 1);
     }
 }
